c0fef46f1676ecd40d37df0e6d4d843eee6082e5
TarikEren <1210606015@nku.edu.tr>
2024-02-05 20:55:37 +0300

    Removed unused functions, created TokenType

--- src/Token.cpp
+++ src/Token.cpp
@@ -1,41 +1,9 @@
 #include "../include/Token.hpp"
 
-string Token::get_TokenStr() const {
-    return this->m_token_str;
-}
-
-[[maybe_unused]] int Token::get_tokenType() const {
-    return this->m_token_type;
-}
-
-[[maybe_unused]] void Token::set_tokenStr(const string &str) {
-    this->m_token_str = str;
-}
-
-[[maybe_unused]] void Token::set_tokenType(int type) {
-    this->m_token_type = type;
-}
-
-[[maybe_unused]] void Token::clear_tokenStr() {
-    this->m_token_str.clear();
-}
-
-[[maybe_unused]] void Token::clear_tokenType() {
-    this->m_token_type = -1;
-}
-
-Token::Token(string token_str, int token_type) {
-    this->m_token_str = std::move(token_str);
+Token::Token(std::optional<string> value, TokenType token_type) {
+    this->m_value = std::move(value);
     this->m_token_type = token_type;
-    string token_type_str = tokenType_to_string();
 }
 
-[[maybe_unused]] Token::Token(const Token &token_obj) {
-    this->m_token_type = token_obj.m_token_type;
-    this->m_token_str = token_obj.m_token_str;
-}
 
-Token::~Token() {
-    this->m_token_str.clear();
-    this->m_token_type = 0;
-}
+Token::~Token() = default;
