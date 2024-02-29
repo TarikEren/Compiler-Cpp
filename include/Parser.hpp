f201d31aca67a57e3483d4e7bdfabe4124b71bf9
TarikEren <1210606015@nku.edu.tr>
2024-03-01 00:15:18 +0300

    Initialised the parser tree classes

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -1,13 +1,29 @@
 #pragma once
 
+#include <optional>
+
 class Node {
 private:
+    Node* child = nullptr;
+    Node* sibling = nullptr;
+    std::optional<int> value{};
 public:
-
+    Node();
+    ~Node();
+    Node* get_child();
+    Node* get_sibling();
+    int get_value();
+    void set_child(Node* new_node);
+    void set_sibling(Node* new_node);
+    void set_value(std::optional<int> value);
 };
 
 class Tree {
 private:
+    Node* root;
 public:
-    
+    Tree();
+    ~Tree();
+    Node* get_root();
+    void set_root(Node* new_node);
 };
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,1 +1,36 @@
 #include "../include/Parser.hpp"
+
+Node* Node::get_child() {
+    return this->child;
+}
+Node* Node::get_sibling() {
+    return this->sibling;
+}
+
+void Node::set_child(Node *new_node) {
+    this->child = new_node;
+}
+
+void Node::set_sibling(Node *new_node) {
+    this->sibling = new_node;
+}
+
+int Node::get_value() {
+    if (this->value.has_value())
+        return this->value.value();
+    else {
+        return (int)NULL;
+    }
+}
+
+void Node::set_value(std::optional<int> value) {
+    this->value = value;
+}
+
+Node *Tree::get_root() {
+    return this->root;
+}
+
+void Tree::set_root(Node* new_node) {
+    this->root = new_node;
+}
